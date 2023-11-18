20ab114083de52910fdca722839d2f898891a917
Afshan01 <afshanalamengg@gmail.com>
2023-11-18 09:35:49 +0530

    added any error

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel Job Board</title>
+  <title>Job Verse</title>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
@@ -45,6 +45,13 @@
       class="my-8 rounded-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
       <p class="font-bold">Success!</p>
       <p>{{ session('success') }}</p>
+    </div>
+  @endif
+  @if (session('error'))
+    <div role="alert"
+      class="my-8 rounded-md border-l-4 border-red-300 bg-red-100 p-4 text-red-700 opacity-75">
+      <p class="font-bold">Error!</p>
+      <p>{{ session('error') }}</p>
     </div>
   @endif
 
